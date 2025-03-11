b4aa943ade83be10d2a7de161b043584c924d06f
Robert Rodarte <robertrodarte02@gmail.com>
2025-03-10 22:17:54 -0700

    Adding comments to lecture code

--- Backend/src/app/core/config.py
+++ Backend/src/app/core/config.py
@@ -2,15 +2,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
 from dotenv import load_dotenv
 
-load_dotenv() # Load the .env file
+# Load the .env file
+load_dotenv()
 
 class Settings(BaseSettings):
     # The database URL which gets populated from the .env file
     DATABASE_URL: str 
+
     # The model config which automatically loads the .env file and populates the variables
     model_config = SettingsConfigDict(env_file = ".env", env_file_encoding = "utf-8") 
 
-@lru_cache
+@lru_cache # Cache the settings to avoid reading the .env file multiple times
 def get_settings() -> Settings:
     return Settings()
 

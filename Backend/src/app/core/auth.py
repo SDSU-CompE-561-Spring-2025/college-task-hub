56f0c3dcd4eceb82e6d2a4f8d5525efc6c8ae1cb
konradekk14 <konradekk14@gmail.com>
2025-04-07 15:01:45 -0700

    authentication functions in auth.py

--- Backend/src/app/core/auth.py
+++ Backend/src/app/core/auth.py
@@ -1,7 +1,27 @@
 from app.core.config import settings
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, UTC
+import jwt
 
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# auth functions
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + ( expires_delta if expires_delta else timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) )
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt

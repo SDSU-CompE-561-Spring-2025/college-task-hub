f57827ff314cb9f2ed5de38fd6371af58383169f
konradekk14 <konradekk14@gmail.com>
2025-05-07 12:25:54 -0700

    docker attempts

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,11 @@
+// app/head.tsx
+export default function Head() {
+	return (
+		<>
+			<title>TaskU</title>
+			<meta name="description" content="TaskU" />
+			<link rel="icon" href="/favicon.ico" />
+
+		</>
+	);
+}

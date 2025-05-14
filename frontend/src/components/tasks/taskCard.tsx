c69c39500754521019c372407e91fabffe222e53
konradekk14 <konradekk14@gmail.com>
2025-05-13 17:58:03 -0700

    addition of shadow

--- frontend/src/components/tasks/taskCard.tsx
+++ frontend/src/components/tasks/taskCard.tsx
@@ -10,7 +10,7 @@ interface TaskCardProps {
 
 export default function TaskCard({ taskName, section, profilePicUrl, description }: TaskCardProps) {
   return (
-    <div className="flex flex-col gap-4 max-w-64 w-full bg-gray-100 p-4 rounded-lg">
+    <div className="flex flex-col gap-4 max-w-64 w-full bg-gray-100 p-4 rounded-lg hover:shadow-lg transition-shadow">
       <h2 className="w-full flow-text break-words text-wrap text-lg text-black">{taskName}</h2>
       {description && <p className="w-full break-words text-wrap text-gray-500 text-sm">{description}</p>}
     </div>
